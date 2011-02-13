9432b10f76baa5a0ff2c61bb8a0b6899a35d5e3f
Wang Pengcheng <devnull@localhost>
2011-02-13 22:09:59 +0000

    Finish make mind map

--- src/it/technical/make.mm
+++ src/it/technical/make.mm
@@ -15,6 +15,15 @@
 <node CREATED="1297633531093" ID="ID_1482522495" MODIFIED="1297633538917" TEXT="&#x5c40;&#x90e8;(&#x76ee;&#x6807;)&#x53d8;&#x91cf;">
 <node CREATED="1297633544675" ID="ID_661066253" MODIFIED="1297633556910" TEXT="target: var-assignments"/>
 </node>
+<node CREATED="1297634865355" ID="ID_959402658" MODIFIED="1297634873147" TEXT="&#x81ea;&#x52a8;&#x5316;&#x53d8;&#x91cf;">
+<node CREATED="1297634911608" ID="ID_420927240" MODIFIED="1297634912687" TEXT="$@ &#x8868;&#x793a;&#x89c4;&#x5219;&#x4e2d;&#x7684;&#x76ee;&#x6807;&#x6587;&#x4ef6;&#x96c6;&#x3002;&#x5728;&#x6a21;&#x5f0f;&#x89c4;&#x5219;&#x4e2d;&#xff0c;&#x5982;&#x679c;&#x6709;&#x591a;&#x4e2a;&#x76ee;&#x6807;&#xff0c;&#x90a3;&#x4e48;&#xff0c;&quot;$@&quot;&#x5c31;&#x662f;&#x5339;&#x914d;&#x4e8e;&#x76ee;&#x6807;&#x4e2d;&#x6a21;&#x5f0f;&#x5b9a;&#x4e49;&#x7684;&#x96c6;&#x5408;&#x3002; "/>
+<node CREATED="1297634915655" ID="ID_1819949221" MODIFIED="1297634916438" TEXT="$% &#x4ec5;&#x5f53;&#x76ee;&#x6807;&#x662f;&#x51fd;&#x6570;&#x5e93;&#x6587;&#x4ef6;&#x4e2d;&#xff0c;&#x8868;&#x793a;&#x89c4;&#x5219;&#x4e2d;&#x7684;&#x76ee;&#x6807;&#x6210;&#x5458;&#x540d;&#x3002;&#x4f8b;&#x5982;&#xff0c;&#x5982;&#x679c;&#x4e00;&#x4e2a;&#x76ee;&#x6807;&#x662f;&quot;foo.a(bar.o)&quot;&#xff0c;&#x90a3;&#x4e48;&#xff0c;&quot;$%&quot;&#x5c31;&#x662f;&quot;bar.o&quot;&#xff0c;&quot;$@&quot;&#x5c31;&#x662f;&quot;foo.a&quot;&#x3002;&#x5982;&#x679c;&#x76ee;&#x6807;&#x4e0d;&#x662f;&#x51fd;&#x6570;&#x5e93;&#x6587;&#x4ef6;&#xff08;Unix&#x4e0b;&#x662f;[.a]&#xff0c;Windows&#x4e0b;&#x662f;[.lib]&#xff09;&#xff0c;&#x90a3;&#x4e48;&#xff0c;&#x5176;&#x503c;&#x4e3a;&#x7a7a;&#x3002; "/>
+<node CREATED="1297634919302" ID="ID_192104444" MODIFIED="1297634920082" TEXT="$&lt; &#x4f9d;&#x8d56;&#x76ee;&#x6807;&#x4e2d;&#x7684;&#x7b2c;&#x4e00;&#x4e2a;&#x76ee;&#x6807;&#x540d;&#x5b57;&#x3002;&#x5982;&#x679c;&#x4f9d;&#x8d56;&#x76ee;&#x6807;&#x662f;&#x4ee5;&#x6a21;&#x5f0f;&#xff08;&#x5373;&quot;%&quot;&#xff09;&#x5b9a;&#x4e49;&#x7684;&#xff0c;&#x90a3;&#x4e48;&quot;$&lt;&quot;&#x5c06;&#x662f;&#x7b26;&#x5408;&#x6a21;&#x5f0f;&#x7684;&#x4e00;&#x7cfb;&#x5217;&#x7684;&#x6587;&#x4ef6;&#x96c6;&#x3002;&#x6ce8;&#x610f;&#xff0c;&#x5176;&#x662f;&#x4e00;&#x4e2a;&#x4e00;&#x4e2a;&#x53d6;&#x51fa;&#x6765;&#x7684;&#x3002; "/>
+<node CREATED="1297634923024" ID="ID_1891119758" MODIFIED="1297634923752" TEXT="$?  &#x6240;&#x6709;&#x6bd4;&#x76ee;&#x6807;&#x65b0;&#x7684;&#x4f9d;&#x8d56;&#x76ee;&#x6807;&#x7684;&#x96c6;&#x5408;&#x3002;&#x4ee5;&#x7a7a;&#x683c;&#x5206;&#x9694;&#x3002; "/>
+<node CREATED="1297634926695" ID="ID_683852265" MODIFIED="1297634927427" TEXT="$^ &#x6240;&#x6709;&#x7684;&#x4f9d;&#x8d56;&#x76ee;&#x6807;&#x7684;&#x96c6;&#x5408;&#x3002;&#x4ee5;&#x7a7a;&#x683c;&#x5206;&#x9694;&#x3002;&#x5982;&#x679c;&#x5728;&#x4f9d;&#x8d56;&#x76ee;&#x6807;&#x4e2d;&#x6709;&#x591a;&#x4e2a;&#x91cd;&#x590d;&#x7684;&#xff0c;&#x90a3;&#x4e2a;&#x8fd9;&#x4e2a;&#x53d8;&#x91cf;&#x4f1a;&#x53bb;&#x9664;&#x91cd;&#x590d;&#x7684;&#x4f9d;&#x8d56;&#x76ee;&#x6807;&#xff0c;&#x53ea;&#x4fdd;&#x7559;&#x4e00;&#x4efd;&#x3002; "/>
+<node CREATED="1297634930177" ID="ID_712628662" MODIFIED="1297634930963" TEXT="$+ &#x8fd9;&#x4e2a;&#x53d8;&#x91cf;&#x5f88;&#x50cf;&quot;$^&quot;&#xff0c;&#x4e5f;&#x662f;&#x6240;&#x6709;&#x4f9d;&#x8d56;&#x76ee;&#x6807;&#x7684;&#x96c6;&#x5408;&#x3002;&#x53ea;&#x662f;&#x5b83;&#x4e0d;&#x53bb;&#x9664;&#x91cd;&#x590d;&#x7684;&#x4f9d;&#x8d56;&#x76ee;&#x6807;&#x3002; "/>
+<node CREATED="1297634934127" ID="ID_812850875" MODIFIED="1297634934944" TEXT="$* &#x8fd9;&#x4e2a;&#x53d8;&#x91cf;&#x8868;&#x793a;&#x76ee;&#x6807;&#x6a21;&#x5f0f;&#x4e2d;&quot;%&quot;&#x53ca;&#x5176;&#x4e4b;&#x524d;&#x7684;&#x90e8;&#x5206; "/>
+</node>
 </node>
 <node CREATED="1297633707697" ID="ID_934303259" MODIFIED="1297633710522" POSITION="left" TEXT="&#x8868;&#x8fbe;&#x5f0f;">
 <node CREATED="1297633726580" ID="ID_252158395" MODIFIED="1297633727374" TEXT="conditional-directive">
@@ -30,7 +39,51 @@
 <node CREATED="1297633776104" ID="ID_403725798" MODIFIED="1297633778814" TEXT="endif"/>
 </node>
 <node CREATED="1297633865496" ID="ID_553683594" MODIFIED="1297633867827" TEXT="function">
-<node CREATED="1297633879466" ID="ID_450460860" MODIFIED="1297633880795" TEXT="$(&lt;function&gt; &lt;arguments&gt; )"/>
+<node CREATED="1297633879466" ID="ID_450460860" MODIFIED="1297634076585" TEXT="$(FUNCTION ARGUMENTS)">
+<node CREATED="1297634076554" ID="ID_401387176" MODIFIED="1297634084280" TEXT="&#x5b57;&#x7b26;&#x4e32;&#x5904;&#x7406;">
+<node CREATED="1297634043924" ID="ID_212980692" MODIFIED="1297634098038" TEXT="$(subst from,to,text) &#x5b57;&#x7b26;&#x4e32;&#x66ff;&#x6362;"/>
+<node CREATED="1297634104869" ID="ID_216956376" MODIFIED="1297634120565" TEXT="$(patsubst pattern,replacement,text) &#x6a21;&#x5f0f;&#x5b57;&#x7b26;&#x4e32;&#x66ff;&#x6362;"/>
+<node CREATED="1297634131226" ID="ID_1755225769" MODIFIED="1297634156782" TEXT="$(strip string) &#x53bb;&#x6389;&#x5934;&#x5c3e;&#x7a7a;&#x683c;"/>
+<node CREATED="1297634164500" ID="ID_856915449" MODIFIED="1297634175496" TEXT="$(findstring find,in)&#x67e5;&#x627e;&#x5b57;&#x7b26;&#x4e32;"/>
+<node CREATED="1297634196088" ID="ID_1505554425" MODIFIED="1297634220368" TEXT="$(filter pattern1 patter2...,text)&#x8fc7;&#x6ee4;&#x51fd;&#x6570;&#xff0c;&#x7b26;&#x5408;&#x6a21;&#x5f0f;&#x7684;&#x7559;&#x4e0b;"/>
+<node CREATED="1297634234475" ID="ID_743253773" MODIFIED="1297634365384" TEXT="$(filter-out pattern1 patter2...,text)&#x53cd;&#x8fc7;&#x6ee4;"/>
+<node CREATED="1297634259981" ID="ID_1123708441" MODIFIED="1297634363750" TEXT="$(sort list) &#x6392;&#x5e8f;"/>
+<node CREATED="1297634274381" ID="ID_290165652" MODIFIED="1297634362108" TEXT="$(word n,text)&#x53d6;&#x5355;&#x8bcd;"/>
+<node CREATED="1297634295784" ID="ID_1218776571" MODIFIED="1297634360290" TEXT="$(wordlist start,end,text) &#x53d6;&#x5355;&#x8bcd;&#x4e32;"/>
+<node CREATED="1297634329985" ID="ID_1738030708" MODIFIED="1297634339166" TEXT="$(words text)&#x5355;&#x8bcd;&#x4e2a;&#x6570;&#x7edf;&#x8ba1;"/>
+<node CREATED="1297634343560" ID="ID_1383684555" MODIFIED="1297634357602" TEXT="$(firstword text) &#x9996;&#x4e2a;&#x5355;&#x8bcd;"/>
+</node>
+<node CREATED="1297634380066" ID="ID_1272900961" MODIFIED="1297634383467" TEXT="&#x6587;&#x4ef6;&#x540d;">
+<node CREATED="1297634389825" ID="ID_130432403" MODIFIED="1297634395296" TEXT="$(dir names...) "/>
+<node CREATED="1297634399058" ID="ID_1918094370" MODIFIED="1297634403977" TEXT="$(notdir names...) "/>
+<node CREATED="1297634413056" ID="ID_733487944" MODIFIED="1297634422630" TEXT="$(suffix names...) &#x53d6;&#x540e;&#x7f00;"/>
+<node CREATED="1297634427231" ID="ID_871067073" MODIFIED="1297634436112" TEXT="$(basename names...)&#x53d6;&#x524d;&#x7f00;"/>
+<node CREATED="1297634440697" ID="ID_1460537597" MODIFIED="1297634446890" TEXT="$(addsuffix suffix,names...) "/>
+<node CREATED="1297634453628" ID="ID_1494555202" MODIFIED="1297634459630" TEXT="$(addprefix prefix,names...) "/>
+<node CREATED="1297634464605" ID="ID_498834705" MODIFIED="1297634470797" TEXT="$(join list1,list2)">
+<node CREATED="1297634483395" ID="ID_1272179247" MODIFIED="1297634523387" TEXT="&#x628a;list2 &#x4e2d;&#x7684;&#x5355;&#x8bcd;&#x5bf9;&#x5e94;&#x5730;&#x52a0;&#x5230;list1&#x7684;&#x5355;&#x8bcd;&#x540e;&#x9762;&#x3002;&#x5982;&#x679c;list1&#x7684;&#x5355;&#x8bcd;&#x4e2a;&#x6570;&#x8981;&#x6bd4;list2&#x7684;&#x591a;&#xff0c;&#x90a3;&#x4e48;list1&#x4e2d;&#x7684;&#x591a;&#x51fa;&#x6765;&#x7684;&#x5355;&#x8bcd;&#x5c06;&#x4fdd;&#x6301;&#x539f;&#x6837;&#x3002;&#x5982;&#x679c;list2&#x7684;&#x5355;&#x8bcd;&#x4e2a;&#x6570;&#x8981;&#x6bd4;list1&#x591a;&#xff0c;&#x90a3;&#x4e48;&#xff0c;list2&#x591a;&#x51fa;&#x6765;&#x7684;&#x5355;&#x8bcd;&#x5c06;&#x88ab;&#x590d;&#x5236;&#x5230;list2&#x4e2d;&#x3002;"/>
+</node>
+</node>
+<node CREATED="1297634561832" ID="ID_841908801" MODIFIED="1297634563941" TEXT="foreach">
+<node CREATED="1297634625032" ID="ID_1466645139" MODIFIED="1297634626958" TEXT="names := a b c d"/>
+<node CREATED="1297634632818" ID="ID_652755378" MODIFIED="1297634633711" TEXT="files := $(foreach n,$(names),$(n).o)"/>
+<node CREATED="1297634639782" ID="ID_114219754" MODIFIED="1297634640612" TEXT="$(files)&#x7684;&#x503c;&#x662f;&#x201c;a.o b.o c.o d.o&#x201d;"/>
+</node>
+<node CREATED="1297634575286" ID="ID_1127505102" MODIFIED="1297634582949" TEXT="$(if condition, then-part) "/>
+<node CREATED="1297634588288" ID="ID_851325131" MODIFIED="1297634597237" TEXT="$(if condition,then-part,else-part)"/>
+<node CREATED="1297634661683" ID="ID_1882501923" MODIFIED="1297634662757" TEXT="call"/>
+<node CREATED="1297634687436" ID="ID_834570897" MODIFIED="1297634690802" TEXT="$(origin variable)">
+<node CREATED="1297634697857" ID="ID_1002181597" MODIFIED="1297634698583" TEXT="undefined"/>
+<node CREATED="1297634699201" ID="ID_1878781826" MODIFIED="1297634700802" TEXT="default"/>
+<node CREATED="1297634706899" ID="ID_86119158" MODIFIED="1297634707476" TEXT="environment"/>
+<node CREATED="1297634712620" ID="ID_1499322318" MODIFIED="1297634713520" TEXT="file"/>
+<node CREATED="1297634717248" ID="ID_126220716" MODIFIED="1297634719167" TEXT="command line"/>
+<node CREATED="1297634722395" ID="ID_91198737" MODIFIED="1297634724324" TEXT="automatic"/>
+</node>
+<node CREATED="1297634748269" ID="ID_1845441262" MODIFIED="1297634754475" TEXT="$(shell command...)"/>
+<node CREATED="1297634763146" ID="ID_1864104789" MODIFIED="1297634767464" TEXT="$(error message)"/>
+<node CREATED="1297634771980" ID="ID_1703284732" MODIFIED="1297634775908" TEXT="$(warning message)"/>
+</node>
 </node>
 </node>
 <node CREATED="1297605299435" ID="ID_922394327" MODIFIED="1297605304228" POSITION="left" TEXT="&#x6587;&#x4ef6;&#x6307;&#x793a;">
@@ -106,8 +159,8 @@
 <node CREATED="1297633081495" ID="ID_177549748" MODIFIED="1297633086390" TEXT="&#x5d4c;&#x5957;&#x6267;&#x884c;"/>
 <node CREATED="1297633109482" ID="ID_537453502" MODIFIED="1297633110946" TEXT="&#x5b9a;&#x4e49;&#x547d;&#x4ee4;&#x5305;">
 <node CREATED="1297633139445" ID="ID_1935372102" MODIFIED="1297633142626" TEXT="define name"/>
-<node CREATED="1297633145573" ID="ID_1305537816" MODIFIED="1297633413995" TEXT="&lt;TAB&gt;command"/>
-<node CREATED="1297633148166" ID="ID_1383232352" MODIFIED="1297633418496" TEXT="&lt;TAB&gt;command"/>
+<node CREATED="1297633145573" ID="ID_1305537816" MODIFIED="1297634017800" TEXT="TAB command"/>
+<node CREATED="1297633148166" ID="ID_1383232352" MODIFIED="1297634024928" TEXT="TAB command"/>
 <node CREATED="1297633152325" ID="ID_1038663057" MODIFIED="1297633153394" TEXT="endef"/>
 </node>
 </node>
