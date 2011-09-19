4dd1c81f0cd4a9556a2d4570084e9f4de56667ca
Wang Pengcheng <devnull@localhost>
2011-09-19 17:52:59 +0800

    Add WHU Courses

---
+++ src/it/study/whu/Courses.mm
@@ -0,0 +1,167 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1316424772546" ID="ID_1134065211" MODIFIED="1316425945937" TEXT="&#x8bfe;&#x7a0b;Courses">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316424843388" ID="ID_303040216" MODIFIED="1316425945936" POSITION="right" TEXT="SSD">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316424907430" ID="ID_744827086" MODIFIED="1316425945933" TEXT="&#x4fe1;&#x606f;&#x7cfb;&#x7edf;&#x6982;&#x8bba;&#xff08;SSD1 Introduction to Information Systems&#xff09;: &#x8be5;&#x8bfe;&#x7a0b;&#x5411;&#x5b66;&#x5458;&#x4ecb;&#x7ecd;&#x8f6f;&#x4ef6;&#x5f00;&#x53d1;&#x7684;&#x57fa;&#x672c;&#x6982;&#x5ff5;&#x548c;&#x6280;&#x5de7;&#x3002; ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316424965136" ID="ID_1428941378" MODIFIED="1316425945925" TEXT="&#x4ec0;&#x4e48;&#x662f;&#x8ba1;&#x7b97;&#x673a;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316424973124" ID="ID_1360545748" MODIFIED="1316425945925" TEXT="&#x786c;&#x4ef6;&#x57fa;&#x7840;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1316424917588" ID="ID_1562389961" MODIFIED="1316425945921" TEXT="&#x8ba1;&#x7b97;&#x673a;&#x7cfb;&#x7edf;&#x6982;&#x8bba;&#xff08;SSD2 Introduction to Computer Systems&#xff09; : &#x8be5;&#x8bfe;&#x7a0b;&#x5411;&#x5b66;&#x5458;&#x4ecb;&#x7ecd;&#x5728;Internet&#x73af;&#x5883;&#x4e0b;&#x4f7f;&#x7528;&#x548c;&#x7ef4;&#x62a4;&#x8ba1;&#x7b97;&#x673a;&#x7cfb;&#x7edf;&#x7684;&#x57fa;&#x7840;&#x77e5;&#x8bc6; ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316424981241" ID="ID_76051516" MODIFIED="1316425945912" TEXT="Java Servlet">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316424987399" ID="ID_1796038" MODIFIED="1316425945912" TEXT="Java Applet">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425013949" ID="ID_459633246" MODIFIED="1316425945911" TEXT="JSP/Web">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425020931" ID="ID_295311221" MODIFIED="1316425945911" TEXT="MySQL">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425025277" ID="ID_1514395963" MODIFIED="1316425945911" TEXT="iBatis"/>
+</node>
+</node>
+<node CREATED="1316424922581" ID="ID_1147964723" MODIFIED="1316425945907" TEXT="&#x9762;&#x5411;&#x5bf9;&#x8c61;&#x7684;&#x7f16;&#x7a0b;&#x4e0e;&#x8bbe;&#x8ba1;&#xff08;SSD3 Object&#xff0d;Oriented Programming and Design&#xff09;: &#x8be5;&#x8bfe;&#x7a0b;&#x5411;&#x5b66;&#x5458;&#x4ecb;&#x7ecd;&#x9762;&#x5411;&#x5bf9;&#x8c61;&#x7684;&#x7a0b;&#x5e8f;&#x8bbe;&#x8ba1;&#x7684;&#x57fa;&#x672c;&#x539f;&#x7406;&#x53ca;&#x5176;&#x5b9e;&#x73b0;&#x7684;&#x65b9;&#x6cd5;&#xff0c;&#x91cd;&#x70b9;&#x653e;&#x5728;&#x9488;&#x5bf9;&#x5177;&#x4f53;&#x95ee;&#x9898;&#x7684;&#x5206;&#x6790;&#x548c;&#x89e3;&#x51b3;&#x65b9;&#x6848;&#x7684;&#x8bbe;&#x8ba1;&#x3001;&#x7a0b;&#x5e8f;&#x7f16;&#x5236;&#x53ca;&#x5b9e;&#x73b0;&#x3002; ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316424993057" ID="ID_807107429" MODIFIED="1316425945900" TEXT="Java">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425001809" ID="ID_1022494040" MODIFIED="1316425945900" TEXT="Thinking In Java"/>
+</node>
+<node CREATED="1316424994688" ID="ID_1743513779" MODIFIED="1316425945899" TEXT="Design Patterns">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1316424930933" ID="ID_1903174548" MODIFIED="1316425945896" TEXT="&#x7528;&#x6237;&#x754c;&#x9762;&#x8bbe;&#x8ba1;&#x4e0e;&#x6d4b;&#x8bd5;&#xff08;SSD4 User-Centered Design and Testing&#xff09;: &#x8be5;&#x8bfe;&#x7a0b;&#x5f3a;&#x8c03;&#x8f6f;&#x4ef6;&#x7684;&#x4eba;&#x673a;&#x4ea4;&#x4e92;&#x6027;&#x80fd;&#xff0c;&#x91cd;&#x70b9;&#x8bad;&#x7ec3;&#x5b66;&#x5458;&#x5728;&#x4efb;&#x52a1;&#x5206;&#x6790;&#x548c;&#x754c;&#x9762;&#x8bc4;&#x4f30;&#x53ca;&#x8bbe;&#x8ba1;&#x65b9;&#x9762;&#x7684;&#x80fd;&#x529b;&#x3002; ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425031704" ID="ID_150927025" MODIFIED="1316425945889" TEXT="VB">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425039234" ID="ID_1543476741" MODIFIED="1316425945888" TEXT="UI">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425041421" ID="ID_1025900759" MODIFIED="1316425945888" TEXT="&#x7528;&#x6237;&#x53ef;&#x7528;&#x6027;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1316424935840" ID="ID_272836389" MODIFIED="1316425945884" TEXT="&#x6570;&#x636e;&#x7ed3;&#x6784;&#x548c;&#x7b97;&#x6cd5;&#xff08;SSD5 Data Structures and Algorithms&#xff09;: &#x672c;&#x8bfe;&#x7a0b;&#x91cd;&#x70b9;&#x5728;&#x4e8e;&#x8ba9;&#x5b66;&#x5458;&#x7406;&#x89e3;&#x6570;&#x636e;&#x7ed3;&#x6784;&#x548c;&#x7b97;&#x6cd5;&#x7684;&#x9009;&#x62e9;&#x5bf9;&#x4e8e;&#x7a0b;&#x5e8f;&#x6267;&#x884c;&#x65f6;&#x95f4;&#x3001;&#x5e26;&#x5bbd;&#x548c;&#x5b58;&#x50a8;&#x91cf;&#x7684;&#x51b3;&#x5b9a;&#x6027;&#x4f5c;&#x7528;&#x3002;&#x5b66;&#x5458;&#x5c06;&#x5728;&#x5b9e;&#x9645;&#x5de5;&#x4f5c;&#x4e2d;&#x81ea;&#x89c9;&#x5730;&#x8003;&#x8651;&#x6570;&#x636e;&#x7ed3;&#x6784;&#x548c;&#x7b97;&#x6cd5;&#x9009;&#x62e9;&#x7684;&#x6b63;&#x786e;&#x6027;&#x548c;&#x590d;&#x6742;&#x6027;&#x95ee;&#x9898;&#x3002; ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425050135" ID="ID_208073870" MODIFIED="1316425945877" TEXT="&#x7b97;&#x6cd5;&#x5bfc;&#x8bba;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425058477" ID="ID_738632761" MODIFIED="1316425945877" TEXT="&#x57fa;&#x7840;&#x7b97;&#x6cd5;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425067145" ID="ID_985233113" MODIFIED="1316425945877" TEXT="&#x6570;&#x636e;&#x7ed3;&#x6784;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425176887" ID="ID_1920350325" MODIFIED="1316425945877" TEXT="Compiler">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425180991" ID="ID_1431753649" MODIFIED="1316425945876" TEXT="&#x89e3;&#x91ca;&#x5668;&#x6784;&#x9020;"/>
+<node CREATED="1316425187710" ID="ID_1088496481" MODIFIED="1316425945876" TEXT="Ruby"/>
+</node>
+</node>
+<node CREATED="1316424941635" ID="ID_1709809590" MODIFIED="1316425945873" TEXT="&#x7cfb;&#x7edf;&#x7f16;&#x7a0b;&#xff08;SSD6 System-Level Programming&#xff09; : &#x672c;&#x8bfe;&#x7a0b;&#x8981;&#x6c42;&#x5b66;&#x5458;&#x7ad9;&#x5728;&#x7a0b;&#x5e8f;&#x89c4;&#x5212;&#x8005;&#x7684;&#x9ad8;&#x5ea6;&#x6765;&#x5bf9;&#x5f85;&#x5904;&#x7406;&#x5668;&#x3001;&#x5b58;&#x50a8;&#x5668;&#x548c;&#x64cd;&#x4f5c;&#x7cfb;&#x7edf;&#x3002; ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425071617" ID="ID_907037147" MODIFIED="1316425945867" TEXT="&#x6df1;&#x5165;&#x7406;&#x89e3;&#x8ba1;&#x7b97;&#x673a;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425076832" ID="ID_736790102" MODIFIED="1316425945866" TEXT="&#x5b9e;&#x7528;&#x7684;&#x8ba1;&#x7b97;&#x673a;&#x4f53;&#x7cfb;&#x7ed3;&#x6784;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425087901" ID="ID_1176931117" MODIFIED="1316425945866" TEXT="C/C++">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1316424946040" ID="ID_1028323336" MODIFIED="1316425945863" TEXT="&#x6570;&#x636e;&#x5e93;&#x7cfb;&#x7edf;&#xff08;SSD7 Database Systems&#xff09; : &#x672c;&#x8bfe;&#x7a0b;&#x5c06;&#x5411;&#x5b66;&#x5458;&#x4ecb;&#x7ecd;&#x6570;&#x636e;&#x5e93;&#x7684;&#x6982;&#x5ff5;&#x548c;&#x8bbe;&#x8ba1;&#x6570;&#x636e;&#x5e93;&#x7684;&#x76f8;&#x5173;&#x77e5;&#x8bc6;&#x3002;&#x5173;&#x7cfb;&#x6570;&#x636e;&#x6a21;&#x578b;&#x5c06;&#x88ab;&#x4f5c;&#x4e3a;&#x6559;&#x5b66;&#x91cd;&#x70b9;&#x3002; ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425103053" ID="ID_1841312251" MODIFIED="1316425945855" TEXT="MySQL">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425107564" ID="ID_876840097" MODIFIED="1316425945855" TEXT="SQL">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425109327" ID="ID_811179940" MODIFIED="1316425945855" TEXT="&#x6570;&#x636e;&#x5e93;&#x8bbe;&#x8ba1;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1316424950854" ID="ID_1280217087" MODIFIED="1316425945850" TEXT="&#x7f51;&#x7edc;&#x4e0e;&#x5206;&#x5e03;&#x5f0f;&#x7cfb;&#x7edf;&#xff08;SSD8 Networks and Distributed Computing&#xff09;: &#x8be5;&#x8bfe;&#x7a0b;&#x6ce8;&#x91cd;&#x8bb2;&#x89e3;&#x7f51;&#x7edc;&#x8ba1;&#x7b97;&#x7684;&#x539f;&#x5219;&#x548c;&#x5b9e;&#x8df5;&#x3002;&#x8bfe;&#x7a0b;&#x662f;&#x4ece;&#x652f;&#x6301;&#x6570;&#x636e;&#x548c;&#x591a;&#x5a92;&#x4f53;&#x901a;&#x8baf;&#x7684;&#x7f51;&#x7edc;&#x6280;&#x672f;&#x63cf;&#x8ff0;&#x5f00;&#x59cb;&#xff0c;&#x518d;&#x5230;&#x9762;&#x5411;&#x5e94;&#x7528;&#x7684;&#x534f;&#x8bae;&#xff0c;&#x6700;&#x540e;&#x662f;&#x57fa;&#x4e8e;Java&#xff0c;&#x9762;&#x5411;&#x5bf9;&#x8c61;&#x7684;&#x5206;&#x5e03;&#x5f0f;&#x7a0b;&#x5e8f;&#x8bbe;&#x8ba1;&#x3002; ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425115464" ID="ID_554765409" MODIFIED="1316425945840" TEXT="UNIX">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425119344" ID="ID_1042214642" MODIFIED="1316425945840" TEXT="Socket">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425123239" ID="ID_1185064457" MODIFIED="1316425945840" TEXT="Web Server">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1316424955611" ID="ID_798282525" MODIFIED="1316425945834" TEXT="&#x8f6f;&#x4ef6;&#x7cfb;&#x7edf;&#x89c4;&#x8303;&#x3001;&#x6d4b;&#x8bd5;&#x548c;&#x7ef4;&#x62a4;&#xff08;SSD9 Software Specification Testing and Maintenance&#xff09;: &#x8be5;&#x8bfe;&#x7a0b;&#x5173;&#x6ce8;&#x57fa;&#x4e8e;&#x8f6f;&#x4ef6;&#x5de5;&#x7a0b;&#x5b9e;&#x8df5;&#x7684;&#x8f6f;&#x4ef6;&#x53d1;&#x5c55;&#x89c4;&#x5f8b;&#x3002;&#x4ee5;&#x72ec;&#x7acb;&#x6216;&#x5408;&#x4f5c;&#x7684;&#x65b9;&#x5f0f;&#xff0c;&#x901a;&#x8fc7;&#x53c2;&#x4e0e;&#x57fa;&#x4e8e;Internet&#x7684;&#x8f6f;&#x4ef6;&#x9879;&#x76ee;&#x7684;&#x5f00;&#x53d1;&#x5de5;&#x4f5c;&#xff0c;&#x5b66;&#x5458;&#x5c06;&#x83b7;&#x5f97;&#x6db5;&#x76d6;&#x8f6f;&#x4ef6;&#x4ea7;&#x54c1;&#x751f;&#x547d;&#x5468;&#x671f;&#x6240;&#x6709;&#x9636;&#x6bb5;&#x7684;&#x5b9d;&#x8d35;&#x7ecf;&#x9a8c;&#x3002; ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425128574" ID="ID_1755946172" MODIFIED="1316425945814" TEXT="JUnit">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425130877" ID="ID_938684848" MODIFIED="1316425945814" TEXT="TDD">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425137815" ID="ID_1085261222" MODIFIED="1316425945814" TEXT="BDD">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1316424959558" ID="ID_842597856" MODIFIED="1316425945808" TEXT="&#x8f6f;&#x4ef6;&#x9879;&#x76ee;&#x7684;&#x7ba1;&#x7406;&#x4e0e;&#x7ec4;&#x7ec7;&#xff08;SSD10 Software Project Organization and Management&#xff09;: &#x8be5;&#x8bfe;&#x7a0b;&#x805a;&#x7126;&#x5728;&#x8f6f;&#x4ef6;&#x9879;&#x76ee;&#x7684;&#x7ba1;&#x7406;&#x548c;&#x7ec4;&#x7ec7;&#x95ee;&#x9898;&#x4e0a;&#x3002;&#x5b66;&#x5458;&#x5c06;&#x901a;&#x8fc7;&#x4ea4;&#x4e92;&#x5f0f;&#x7684;&#x89c2;&#x5bdf;&#xff0c;&#x68c0;&#x67e5;&#x548c;&#x6bd4;&#x8f83;&#x5404;&#x79cd;&#x5404;&#x6837;&#x7684;&#x8ba1;&#x5212;&#x548c;&#x7ba1;&#x7406;&#x65b9;&#x6848;&#xff0c;&#x5b66;&#x4e60;&#x5230;&#x8f6f;&#x4ef6;&#x9879;&#x76ee;&#x7684;&#x8ba1;&#x5212;&#x3001;&#x5de5;&#x4f5c;&#x65e5;&#x7a0b;&#x5b89;&#x6392;&#x3001;&#x6210;&#x672c;&#x6838;&#x7b97;&#x548c;&#x7ec4;&#x7ec7;&#x7684;&#x65b9;&#x6cd5;&#x3002;  ">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425142464" ID="ID_1098490245" MODIFIED="1316425945798" TEXT="BDD">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425148620" ID="ID_707515265" MODIFIED="1316425945797" TEXT="&#x654f;&#x6377;&#x8f6f;&#x4ef6;&#x5f00;&#x53d1;">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425159058" ID="ID_29289775" MODIFIED="1316425945797" TEXT="RoR"/>
+</node>
+</node>
+</node>
+<node CREATED="1316425204335" ID="ID_1650464387" MODIFIED="1316425945797" POSITION="right" TEXT="&#x9488;&#x5bf9;&#x6027;&#x5b66;&#x4e60;">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425212940" ID="ID_596572881" MODIFIED="1316425945797" TEXT="&#x6570;&#x5b57;&#x56fe;&#x8c61;&#x5904;&#x7406;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425217453" ID="ID_1187110998" MODIFIED="1316425945797" TEXT="&#x6a21;&#x5f0f;&#x8bc6;&#x522b;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425223823" ID="ID_14052128" MODIFIED="1316425945796" TEXT="&#x5d4c;&#x5165;&#x5f0f;&#x5f00;&#x53d1;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425850029" ID="ID_144992886" MODIFIED="1316425945796" TEXT="&#x6570;&#x5b57;&#x903b;&#x8f91;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1316425866661" ID="ID_447199367" MODIFIED="1316425945796" POSITION="right" TEXT="&#x6570;&#x5b66;&#x57fa;&#x7840;">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425870782" ID="ID_303617408" MODIFIED="1316425945795" TEXT="&#x9ad8;&#x6570;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425873619" ID="ID_550360802" MODIFIED="1316425945795" TEXT="&#x7ebf;&#x6027;&#x4ee3;&#x6570;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425879008" ID="ID_957215903" MODIFIED="1316425945795" TEXT="&#x6982;&#x7387;&#x8bba;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1316425884118" ID="ID_1346162460" MODIFIED="1316425945795" TEXT="&#x79bb;&#x6563;&#x6570;&#x5b66;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1316425891394" ID="ID_1552792933" MODIFIED="1316425945794" POSITION="right" TEXT="&#x5916;&#x8bed;&#x57fa;&#x7840;">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1316425895700" ID="ID_171583920" MODIFIED="1316425945794" TEXT="&#x542c;&#x8bf4;&#x8bfb;&#x5199;">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</map>
