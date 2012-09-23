478f6feb8bd48243be88598c508ee845c62bc937
Wang Pengcheng <qianjigui@gmail.com>
2012-09-23 21:50:23 +0800

    Create a tree for Ruby

---
+++ src/it/technical/language/ruby/RubyAuthorThinking.mm
@@ -0,0 +1,4 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1348408132883" ID="ID_1797621214" MODIFIED="1348408142908" TEXT="RubyAuthorThinking"/>
+</map>
