7645beee96f1259790e0cbd0e6b08244713548d3
Wang Pengcheng <qianjigui@gmail.com>
2012-12-11 22:52:41 +0800

    Add the reading note for Java Harmony project

---
+++ src/it/technical/language/java/JavaHarmony.mm
@@ -0,0 +1,6 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1355235790870" ID="ID_1019984678" MODIFIED="1355235803874" TEXT="JavaHarmony">
+<node CREATED="1355235824551" ID="ID_1554900448" MODIFIED="1355235851047" POSITION="right" TEXT="[ReadingRes](http://www.ibm.com/developerworks/cn/java/j-harmony/)"/>
+</node>
+</map>
