c08e7168df4624285dbcdce7bec2d7b1c17f1408
qianjigui <qianjigui@gmail.com>
2014-09-10 08:25:12 +0800

    Add review for fitness

--- src/it/project/FinancialManage/StockManage.mm
+++ src/it/project/FinancialManage/StockManage.mm
@@ -1,19 +1,56 @@
 <map version="1.0.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1409703389816" ID="ID_1874065613" MODIFIED="1409789260082" TEXT="StockManage">
-<node CREATED="1409703520283" ID="ID_1984919383" MODIFIED="1409703529885" POSITION="right" TEXT="&#x4ea4;&#x6613;&#x901a;&#x9053;">
-<node CREATED="1409703453677" ID="ID_183475670" MODIFIED="1409703454738" TEXT="https://www.ibkr.com.cn/cn/main.php">
-<node CREATED="1409703563624" ID="ID_452850780" MODIFIED="1409703572140" TEXT="&#x8bc1;&#x5238;&#x901a;&#x9053;"/>
+<node COLOR="#000000" CREATED="1409703389816" ID="ID_1874065613" MODIFIED="1410307869905" TEXT="StockManage">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1409703520283" ID="ID_1984919383" MODIFIED="1410307869895" POSITION="right" TEXT="&#x4ea4;&#x6613;&#x901a;&#x9053;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1409703453677" ID="ID_183475670" MODIFIED="1410307869896" TEXT="https://www.ibkr.com.cn/cn/main.php">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1409703563624" ID="ID_452850780" MODIFIED="1410307869896" TEXT="&#x8bc1;&#x5238;&#x901a;&#x9053;">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
-<node CREATED="1409703533809" ID="ID_807507419" MODIFIED="1409703561125" TEXT="http://xueqiu.com/">
-<node CREATED="1409703573112" ID="ID_1391769033" MODIFIED="1409703583299" TEXT="&#x4fe1;&#x606f;&#x6c47;&#x603b;&#x901a;&#x9053;"/>
+</node>
+<node COLOR="#00b439" CREATED="1409703533809" ID="ID_807507419" MODIFIED="1410307869897" TEXT="http://xueqiu.com/">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1409703573112" ID="ID_1391769033" MODIFIED="1410307869898" TEXT="&#x4fe1;&#x606f;&#x6c47;&#x603b;&#x901a;&#x9053;">
+<font NAME="SansSerif" SIZE="14"/>
 </node>
 </node>
-<node CREATED="1409789261399" ID="ID_554694554" MODIFIED="1409789266324" POSITION="left" TEXT="&#x9014;&#x5f84;">
-<node CREATED="1409789266690" ID="ID_1633720326" MODIFIED="1409789275309" TEXT="P2P&#x8d37;&#x6b3e;"/>
-<node CREATED="1409789276297" ID="ID_1836797589" MODIFIED="1409789287316" TEXT="&#x80a1;&#x7968;"/>
-<node CREATED="1409789288215" ID="ID_1854485186" MODIFIED="1409789292330" TEXT="&#x7406;&#x8d22;&#x4ea7;&#x54c1;"/>
-<node CREATED="1409789309907" ID="ID_1871001382" MODIFIED="1409789319891" TEXT="&#x57fa;&#x91d1;"/>
+</node>
+<node COLOR="#0033ff" CREATED="1409789261399" ID="ID_554694554" MODIFIED="1410307895262" POSITION="left" TEXT="&#x9014;&#x5f84;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1409789266690" ID="ID_1633720326" MODIFIED="1410307869901" TEXT="P2P&#x8d37;&#x6b3e;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1409789276297" ID="ID_1836797589" MODIFIED="1410307869902" TEXT="&#x80a1;&#x7968;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1409789288215" ID="ID_1854485186" MODIFIED="1410307869903" TEXT="&#x7406;&#x8d22;&#x4ea7;&#x54c1;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1409789309907" ID="ID_1871001382" MODIFIED="1410307869903" TEXT="&#x57fa;&#x91d1;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1410307793602" ID="ID_1223806701" MODIFIED="1410307869904" POSITION="left" TEXT="&#x4e3b;&#x8981;&#x8981;&#x6c42;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1410307797674" ID="ID_914966034" MODIFIED="1410307869905" TEXT="&#x8d44;&#x91d1;&#x6d41;&#x8f6c;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1410307804026" ID="ID_1240708365" MODIFIED="1410307869905" TEXT="&#x6bcf;&#x4e2a;&#x4eba;&#x6bcf;&#x5e74;50000&#x7f8e;&#x91d1;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
 </node>
 </node>
 </map>
