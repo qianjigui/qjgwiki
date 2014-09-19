1829952944dc95336936219af8d0c571c57b281a
qianjigui <qianjigui@gmail.com>
2014-09-19 14:05:28 +0800

    Add GoPackageBuiltin read note

---
+++ src/it/technical/language/go/GoPackageBuiltin.mm
@@ -0,0 +1,9 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1410346880625" ID="ID_496274757" MODIFIED="1410346947733" TEXT="GoPackageBuiltin">
+<node CREATED="1410346983577" ID="ID_1039534427" MODIFIED="1410346995994" POSITION="right" TEXT="Constants"/>
+<node CREATED="1410346999662" ID="ID_1993313401" MODIFIED="1410347005455" POSITION="left" TEXT="Variables"/>
+<node CREATED="1410347008415" ID="ID_52451749" MODIFIED="1410347010062" POSITION="right" TEXT="func"/>
+<node CREATED="1410347011222" ID="ID_1223748102" MODIFIED="1410347012526" POSITION="left" TEXT="type"/>
+</node>
+</map>
