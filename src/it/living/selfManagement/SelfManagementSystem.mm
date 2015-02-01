3abc57eab072c6c3f69816b7dee1dfd567c1e707
qianjigui <qianjigui@gmail.com>
2015-02-01 12:29:27 +0800

    Add review for SelfManagementSystem

--- src/it/living/selfManagement/SelfManagementSystem.mm
+++ src/it/living/selfManagement/SelfManagementSystem.mm
@@ -1,14 +1,81 @@
 <map version="1.0.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1422368908719" ID="ID_568227316" MODIFIED="1422368921246" TEXT="SelfManagementSystem">
-<node CREATED="1422368922874" ID="ID_1206029298" MODIFIED="1422368963217" POSITION="right" TEXT="tags: Review"/>
-<node CREATED="1422368979815" ID="ID_1195597686" MODIFIED="1422368989597" POSITION="left" TEXT="&#x5065;&#x5eb7;&#x7ba1;&#x7406;"/>
-<node CREATED="1422368990344" ID="ID_771050385" MODIFIED="1422369002042" POSITION="left" TEXT="&#x65f6;&#x95f4;&#x7ba1;&#x7406;"/>
-<node CREATED="1422369020582" ID="ID_327058871" MODIFIED="1422369023475" POSITION="left" TEXT="&#x8d22;&#x52a1;&#x7ba1;&#x7406;"/>
-<node CREATED="1422369003325" ID="ID_1429002050" MODIFIED="1422369012274" POSITION="right" TEXT="&#x4e60;&#x60ef;&#x4e0e;&#x6027;&#x683c;&#x7ba1;&#x7406;"/>
-<node CREATED="1422369014846" ID="ID_1392204881" MODIFIED="1422369018731" POSITION="right" TEXT="&#x60c5;&#x7eea;&#x7ba1;&#x7406;"/>
-<node CREATED="1422369047874" ID="ID_123094551" MODIFIED="1422369054183" POSITION="right" TEXT="&#x4eba;&#x8109;&#x7ba1;&#x7406;"/>
-<node CREATED="1422369055978" ID="ID_812235863" MODIFIED="1422369084272" POSITION="left" TEXT="&#x76ee;&#x6807;&#x7ba1;&#x7406;"/>
-<node CREATED="1422369095187" ID="ID_1242618529" MODIFIED="1422369098544" POSITION="right" TEXT="&#x5bb6;&#x5ead;&#x7ba1;&#x7406;"/>
+<node COLOR="#000000" CREATED="1422368908719" ID="ID_568227316" MODIFIED="1422764875962" TEXT="SelfManagementSystem">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1422368922874" ID="ID_1206029298" MODIFIED="1422764875951" POSITION="right" TEXT="tags: Review">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1422368979815" ID="ID_1195597686" MODIFIED="1422764875952" POSITION="left" TEXT="&#x5065;&#x5eb7;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1422764752169" ID="ID_789282627" MODIFIED="1422764875952" TEXT="&#x5065;&#x8eab;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1422764755896" ID="ID_1208611859" MODIFIED="1422764875953" TEXT="&#x996e;&#x98df;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1422368990344" ID="ID_771050385" MODIFIED="1422764875953" POSITION="left" TEXT="&#x65f6;&#x95f4;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1422764766480" ID="ID_28326607" MODIFIED="1422764875954" TEXT="GTD&#x4e8b;&#x4ef6;&#x7ba1;&#x7406;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1422764800738" ID="ID_1289782233" MODIFIED="1422764875954" TEXT="&#x4e8b;&#x60c5;&#x5206;&#x7c7b;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1422764782171" ID="ID_266092905" MODIFIED="1422764875955" TEXT="&#x6d93;&#x6ef4;&#x5217;&#x8868;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1422764807058" ID="ID_1971023561" MODIFIED="1422764875955" TEXT="&#x91cd;&#x8981;&#x4f46;&#x4e0d;&#x7d27;&#x6025;&#x7684;&#x4e8b;&#x60c5;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1422764841836" ID="ID_1978913214" MODIFIED="1422764875956" TEXT="&#x65e9;&#x95f4;&#x65f6;&#x95f4;&#x5229;&#x7528;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1422369020582" ID="ID_327058871" MODIFIED="1422764875957" POSITION="left" TEXT="&#x8d22;&#x52a1;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1422764837084" ID="ID_916775040" MODIFIED="1422764875957" TEXT="&#x8bb0;&#x8d26;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1422764853685" ID="ID_1806026949" MODIFIED="1422764875958" TEXT="&#x7406;&#x8d22;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1422369003325" ID="ID_1429002050" MODIFIED="1422764875959" POSITION="right" TEXT="&#x4e60;&#x60ef;&#x4e0e;&#x6027;&#x683c;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1422369014846" ID="ID_1392204881" MODIFIED="1422764875959" POSITION="right" TEXT="&#x60c5;&#x7eea;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1422369047874" ID="ID_123094551" MODIFIED="1422764875960" POSITION="right" TEXT="&#x4eba;&#x8109;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1422369055978" ID="ID_812235863" MODIFIED="1422764875961" POSITION="left" TEXT="&#x76ee;&#x6807;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1422764931987" ID="ID_244302960" MODIFIED="1422764936016" TEXT="&#x7a81;&#x51fa;&#x91cd;&#x70b9;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1422369095187" ID="ID_1242618529" MODIFIED="1422764875962" POSITION="right" TEXT="&#x5bb6;&#x5ead;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
 </node>
 </map>
