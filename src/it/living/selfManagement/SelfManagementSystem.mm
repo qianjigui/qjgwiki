e07e52c4adcda50f1601ef49936c2da80b10dac3
qianjigui <qianjigui@gmail.com>
2015-02-03 09:31:09 +0800

    Add review for SelfManagementSystem

--- src/it/living/selfManagement/SelfManagementSystem.mm
+++ src/it/living/selfManagement/SelfManagementSystem.mm
@@ -39,6 +39,13 @@
 <node COLOR="#00b439" CREATED="1422764841836" ID="ID_1978913214" MODIFIED="1422764875956" TEXT="&#x65e9;&#x95f4;&#x65f6;&#x95f4;&#x5229;&#x7528;">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1422926506040" ID="ID_1737610677" MODIFIED="1422926509040" TEXT="Review">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1422926509598" ID="ID_581553792" MODIFIED="1422926523173" TEXT="&#x65e9;&#x4e0a;&#x505a;&#x996d;&#x65f6;&#x95f4;&#x5728;50min&#x5de6;&#x53f3;"/>
+<node COLOR="#111111" CREATED="1422926719509" ID="ID_1217820406" MODIFIED="1422926732339" TEXT="&#x65e9;&#x4e0a;&#x505a;&#x7cfb;&#x7edf;&#x6846;&#x67b6;&#x578b;&#x4e8b;&#x60c5;">
+<node COLOR="#111111" CREATED="1422926733205" ID="ID_206481023" MODIFIED="1422926753482" TEXT="&#x5c06;&#x9879;&#x76ee;&#x5206;&#x6210;&#x4efb;&#x52a1;,&#x653e;&#x5728;&#x6bcf;&#x5929;"/>
+</node>
+</node>
 </node>
 </node>
 <node COLOR="#0033ff" CREATED="1422369020582" ID="ID_327058871" MODIFIED="1422764875957" POSITION="left" TEXT="&#x8d22;&#x52a1;&#x7ba1;&#x7406;">
@@ -51,6 +58,9 @@
 <node COLOR="#00b439" CREATED="1422764853685" ID="ID_1806026949" MODIFIED="1422764875958" TEXT="&#x7406;&#x8d22;">
 <edge STYLE="bezier" WIDTH="thin"/>
 <font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1422926535519" ID="ID_134594130" MODIFIED="1422926592377" TEXT="&#x5173;&#x6ce8;&#x81ea;&#x5df1;&#x7684;&#x4e3b;&#x8981;&#x7cbe;&#x529b;,&#x7406;&#x8d22;&#x53ea;&#x662f;&#x4e00;&#x79cd;&#x8d26;&#x52a1;&#x7ba1;&#x7406;&#x65b9;&#x6cd5;.&#x6211;&#x4eec;&#x5e94;&#x8be5;&#x5c06;&#x4e3b;&#x8981;&#x7cbe;&#x529b;&#x653e;&#x5728;&#x79d1;&#x6280;&#x4e92;&#x8054;&#x7f51;&#x7b49;&#x4e8b;&#x60c5;&#x4e0a;, &#x7406;&#x8d22;&#x4ea4;&#x7ed9;&#x4e13;&#x4eba;&#x6253;&#x7406;, &#x6211;&#x4eec;&#x4e3b;&#x8981;&#x4e70;&#x4ea7;&#x54c1;&#x5c31;&#x597d;.">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
 </node>
 </node>
 <node COLOR="#0033ff" CREATED="1422369003325" ID="ID_1429002050" MODIFIED="1422764875959" POSITION="right" TEXT="&#x4e60;&#x60ef;&#x4e0e;&#x6027;&#x683c;&#x7ba1;&#x7406;">
@@ -76,6 +86,29 @@
 <node COLOR="#0033ff" CREATED="1422369095187" ID="ID_1242618529" MODIFIED="1422764875962" POSITION="right" TEXT="&#x5bb6;&#x5ead;&#x7ba1;&#x7406;">
 <edge STYLE="sharp_bezier" WIDTH="8"/>
 <font NAME="SansSerif" SIZE="18"/>
+</node>
+<node COLOR="#0033ff" CREATED="1422926613196" ID="ID_1949842148" MODIFIED="1422926616338" POSITION="right" TEXT="&#x77e5;&#x8bc6;&#x7ba1;&#x7406;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1422926630855" ID="ID_443233155" MODIFIED="1422926638292" TEXT="&#x5173;&#x6ce8;&#x9886;&#x57df;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1422926638599" ID="ID_89732541" MODIFIED="1422926646061" TEXT="&#x79d1;&#x6280;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1422926646304" ID="ID_270260282" MODIFIED="1422926648653" TEXT="&#x4e92;&#x8054;&#x7f51;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1422926648848" ID="ID_1492108871" MODIFIED="1422926813376" TEXT="&#x56e2;&#x961f;&#x7ba1;&#x7406;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1422926660064" ID="ID_778398284" MODIFIED="1422926665422" TEXT="&#x9488;&#x5bf9;&#x7406;&#x8d22;&#x7684;&#x5173;&#x6ce8;">
+<font NAME="SansSerif" SIZE="14"/>
+<node COLOR="#111111" CREATED="1422926665809" ID="ID_1152330103" MODIFIED="1422926672374" TEXT="&#x901a;&#x8fc7;&#x4e92;&#x8054;&#x7f51;&#x5165;&#x624b;">
+<node COLOR="#111111" CREATED="1422926676209" ID="ID_1473912836" MODIFIED="1422926682103" TEXT="&#x5173;&#x6ce8;&#x4e92;&#x8054;&#x7f51;&#x80a1;&#x7968;"/>
+</node>
+</node>
+</node>
 </node>
 </node>
 </map>
